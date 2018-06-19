18661b6b4e45d946cb61a9e6fb46e1da7dbd31dd
crewcrypto <40144460+crewcrypto@users.noreply.github.com>
2018-06-19 02:17:01 +0200

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -15,6 +15,7 @@ typedef std::map<int, unsigned int> MapModifierCheckpoints;
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
         ( 0, 0x0e00670b )
+        ( 100, 0x778853f0 )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
